d0002b20782f8a21a9383e75ced68f7b7c81d30a
Robert Przybylski <robert.przybylski.95@gmail.com>
2016-03-08 19:27:12 +0100

    poprawa dźwięku 6

--- www/js/index.js
+++ www/js/index.js
@@ -574,7 +574,7 @@ function tellMe(){
 	var src = '/android_asset/www/date/' + idLang + "/" + idParentCat + "/" + idSubCat + "/sound/" + id + ".m4a";
 	my_media = new Media(src,
             // success callback
-             function () { console.log("playAudio():Audio Success");mediaRes.release(); },
+             function () { this.release(); },
             // error callback
              function (err) { console.log("M: " + err.message + " - " + err.code); }
     );
