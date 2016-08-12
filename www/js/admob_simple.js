74113e778231bcf4dd11f9d889ca658068ca7338
Robert Przybylski <robert.przybylski.95@gmail.com>
2016-08-12 20:45:20 +0200

    test reklam

---
+++ www/js/admob_simple.js
@@ -0,0 +1,19 @@
+var admobid = {};
+
+// TODO: replace the following ad units with your own
+if( /(android)/i.test(navigator.userAgent) ) { 
+  admobid = { // for Android
+    banner: 'ca-app-pub-6869992474017983/9375997553',
+    interstitial: 'ca-app-pub-6869992474017983/1657046752'
+  };
+} else if(/(ipod|iphone|ipad)/i.test(navigator.userAgent)) {
+  admobid = { // for iOS
+    banner: 'ca-app-pub-6869992474017983/4806197152',
+    interstitial: 'ca-app-pub-6869992474017983/7563979554'
+  };
+} else {
+  admobid = { // for Windows Phone
+    banner: 'ca-app-pub-6869992474017983/8878394753',
+    interstitial: 'ca-app-pub-6869992474017983/1355127956'
+  };
+}
