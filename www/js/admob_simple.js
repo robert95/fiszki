93201ef6a18f264f10b26bd45162bf11078b7d2c
Robert Przybylski <robert.przybylski.95@gmail.com>
2016-08-13 11:58:00 +0200

    test reklamy3

--- www/js/admob_simple.js
+++ www/js/admob_simple.js
@@ -1,16 +1,27 @@
 // display the banner at startup
 function baner(){
 	alert("baner");
-	initAd(); 
-	admob.createBannerView();
+	var admobid = {};
+	// select the right Ad Id according to platform
+	if( /(android)/i.test(navigator.userAgent) ) { 
+		admobid = { // for Android
+			banner: 'ca-app-pub-6869992474017983/9375997553',
+			interstitial: 'ca-app-pub-6869992474017983/1657046752'
+		};
+	} else if(/(ipod|iphone|ipad)/i.test(navigator.userAgent)) {
+		admobid = { // for iOS
+			banner: 'ca-app-pub-6869992474017983/4806197152',
+			interstitial: 'ca-app-pub-6869992474017983/7563979554'
+		};
+	} else {
+		admobid = { // for Windows Phone
+			banner: 'ca-app-pub-6869992474017983/8878394753',
+			interstitial: 'ca-app-pub-6869992474017983/1355127956'
+		};
+	}
 }
 function baner2(){
 	alert("baner2");
-	// Set AdMobAds options:
-      admob.setOptions({
-        publisherId:          "ca-app-pub-5149183983154394~4884180664"  // Required
-      });
-
-      // Start showing banners (atomatic when autoShowBanner is set to true)
+	if(AdMob) AdMob.createBanner( admobid.banner );
      
 }
