6ab54d95569eaf9ef1e75dba1cb13bec108efb10
Robert Przybylski <robert.przybylski.95@gmail.com>
2016-08-13 02:29:51 +0200

    test reklam2

--- www/js/admob_simple.js
+++ www/js/admob_simple.js
@@ -1,23 +1,45 @@
-window.plugins.AdMob.setOptions( {
-  publisherId: 'ca-app-pub-5149183983154394/3906975069',
-  interstitialAdId: 'ca-app-pub-5149183983154394/6360913863',
-  bannerAtTop: false, // set to true, to put banner at top
-  overlap: false, // set to true, to allow banner overlap webview
-  offsetTopBar: false, // set to true to avoid ios7 status bar overlap
-  isTesting: true, // receiving test ad
-  autoShow: true // auto show interstitial ad when loaded
-});
 // display the banner at startup
 function baner(){
 	alert("baner");
-	window.plugins.AdMob.createBannerView();
+	initAd();
 }
 function baner2(){
 	alert("baner2");
-	window.plugins.AdMob.createInterstitialView();
-	window.plugins.AdMob.showInterstitialAd(
-	  true, 
-	  function(){},
-	  function(e){alert(JSON.stringify(e));}
-	);
+	window.plugins.AdMob.createBannerView();
+}
+
+function initAd(){
+	if ( window.plugins && window.plugins.AdMob ) {
+		window.plugins.AdMob.setOptions( {
+			publisherId: 'ca-app-pub-5149183983154394/3906975069',
+			interstitialAdId: 'ca-app-pub-5149183983154394/6360913863',
+			adSize: window.plugins.AdMob.AD_SIZE.SMART_BANNER,	//use SMART_BANNER, BANNER, IAB_MRECT, IAB_BANNER, IAB_LEADERBOARD 
+			bannerAtTop: false, // set to true, to put banner at top 
+			overlap: true, // banner will overlap webview  
+			offsetTopBar: false, // set to true to avoid ios7 status bar overlap 
+			isTesting: false, // receiving test ad 
+			autoShow: false // auto show interstitial ad when loaded 
+		});
+
+		registerAdEvents();
+		window.plugins.AdMob.createInterstitialView();	//get the interstitials ready to be shown 
+		window.plugins.AdMob.requestInterstitialAd();
+
+	} else {
+		alert( 'admob plugin not ready' ); 
+	}
+}
+//functions to allow you to know when ads are shown, etc. 
+function registerAdEvents() {
+	document.addEventListener('onReceiveAd', function(){});
+	document.addEventListener('onFailedToReceiveAd', function(data){});
+	document.addEventListener('onPresentAd', function(){});
+	document.addEventListener('onDismissAd', function(){ });
+	document.addEventListener('onLeaveToAd', function(){ });
+	document.addEventListener('onReceiveInterstitialAd', function(){ });
+	document.addEventListener('onPresentInterstitialAd', function(){ });
+	document.addEventListener('onDismissInterstitialAd', function(){
+		window.plugins.AdMob.createInterstitialView();			//REMOVE THESE 2 LINES IF USING AUTOSHOW 
+		window.plugins.AdMob.requestInterstitialAd();			//get the next one ready only after the current one is closed 
+	});
 }
