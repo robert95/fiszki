7da436dff2665910deb06811a0b14d3eba0a0c54
Robert Przybylski <robert.przybylski.95@gmail.com>
2016-08-13 12:17:23 +0200

    test reklamy3

--- www/js/admob_simple.js
+++ www/js/admob_simple.js
@@ -5,8 +5,8 @@ function baner(){
 	// select the right Ad Id according to platform
 	if( /(android)/i.test(navigator.userAgent) ) { 
 		admobid = { // for Android
-			banner: 'ca-app-pub-6869992474017983/9375997553',
-			interstitial: 'ca-app-pub-6869992474017983/1657046752'
+			banner: 'ca-app-pub-5149183983154394/3906975069',
+			interstitial: 'ca-app-pub-5149183983154394/6360913863'
 		};
 	} else if(/(ipod|iphone|ipad)/i.test(navigator.userAgent)) {
 		admobid = { // for iOS
