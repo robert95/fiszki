c4cabed76b500d3c6e9231269c83a64553d0de1e
Robert Przybylski <robert.przybylski.95@gmail.com>
2016-08-13 11:35:25 +0200

    test reklamy3

--- www/js/admob_simple.js
+++ www/js/admob_simple.js
@@ -1,45 +1,16 @@
 // display the banner at startup
 function baner(){
 	alert("baner");
-	initAd();
+	initAd(); 
+	admob.createBannerView();
 }
 function baner2(){
 	alert("baner2");
-	window.plugins.AdMob.createBannerView();
-}
+	// Set AdMobAds options:
+      admob.setOptions({
+        publisherId:          "ca-app-pub-5149183983154394~4884180664"  // Required
+      });
 
-function initAd(){
-	if ( window.plugins && window.plugins.AdMob ) {
-		window.plugins.AdMob.setOptions( {
-			publisherId: 'ca-app-pub-5149183983154394/3906975069',
-			interstitialAdId: 'ca-app-pub-5149183983154394/6360913863',
-			adSize: window.plugins.AdMob.AD_SIZE.SMART_BANNER,	//use SMART_BANNER, BANNER, IAB_MRECT, IAB_BANNER, IAB_LEADERBOARD 
-			bannerAtTop: true, // set to true, to put banner at top 
-			overlap: true, // banner will overlap webview  
-			offsetTopBar: false, // set to true to avoid ios7 status bar overlap 
-			isTesting: true, // receiving test ad 
-			autoShow: false // auto show interstitial ad when loaded 
-		});
-
-		registerAdEvents();
-		window.plugins.AdMob.createInterstitialView();	//get the interstitials ready to be shown 
-		window.plugins.AdMob.requestInterstitialAd();
-
-	} else {
-		alert( 'admob plugin not ready' ); 
-	}
-}
-//functions to allow you to know when ads are shown, etc. 
-function registerAdEvents() {
-	document.addEventListener('onReceiveAd', function(){});
-	document.addEventListener('onFailedToReceiveAd', function(data){});
-	document.addEventListener('onPresentAd', function(){});
-	document.addEventListener('onDismissAd', function(){ });
-	document.addEventListener('onLeaveToAd', function(){ });
-	document.addEventListener('onReceiveInterstitialAd', function(){ });
-	document.addEventListener('onPresentInterstitialAd', function(){ });
-	document.addEventListener('onDismissInterstitialAd', function(){
-		window.plugins.AdMob.createInterstitialView();			//REMOVE THESE 2 LINES IF USING AUTOSHOW 
-		window.plugins.AdMob.requestInterstitialAd();			//get the next one ready only after the current one is closed 
-	});
+      // Start showing banners (atomatic when autoShowBanner is set to true)
+     
 }
