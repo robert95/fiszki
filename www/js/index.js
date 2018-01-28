a39df32811027cf031b83731e7d238c06bf2d699
Robert Przybylski <robert.przybylski.95@gmail.com>
2018-01-28 23:28:30 +0100

    test android 4.4

--- www/js/index.js
+++ www/js/index.js
@@ -1,3 +1,62 @@
+
+t_not_connected_text = ['Make sure that your Wi-Fi or cellular mobile data is turned on and then try again.', 'Upewnij się, że Wifi lub  dane komórkowe są włączone, a następnie spróbuj ponownie.', 'Asegúrate que el WiFi o datos móviles están activados e inténtalo otra vez.', 'Vergewissere dich, dass dein WLAN oder Mobilfunknetz eingeschaltet ist und versuche es dann nochmal.', 'Assurez-vous que le Wi-Fi ou les données mobiles sont activées et essayez de nouveau.'];
+t_not_connected_title = ['No internet connection', 'Nie można połączyć się z internetem', 'No hay conexión a internet', 'Keine Internetverbindung', 'Pas de connexion Internet'];
+t_not_connected_exit = ['OK', 'OK', 'OK', 'OK', 'OK'];
+t_during_learning = ['Lesson in progress', 'W trakcie nauki', 'Lección en progreso', 'Lektion im Gange', 'Leçon en cours'];
+t_lesson_skipped = ['Skipped', 'Pominięta', 'Omitida', 'Übersprungen', 'Passée'];
+t_learned_lesson = ['Completed', 'Ukończona', 'Finalizada', 'Abgeschlossen', 'Terminée'];
+t_recognize_error_title = ["Didn't catch that", 'Nie zrozumieliśmy', 'No lo entendemos', 'Das habe ich nicht verstanden', "Nous n'avons pas compris"];
+t_recognize_error_try_again = ['Try again', 'Spróbuj ponownie', 'Inténtalo de nuevo', 'Versuche es nochmal', 'Essayez de nouveau'];
+t_recognize_error_skip = ['Skip the task', 'Pomiń zadanie', 'Omitir tarea', 'Aufgabe überspringen', "Passez l'exercice"];
+t_recognize_error_first_text = ['Try to speak more clearly and loudly.', 'Staraj się mówić głośno i wyraźnie.', 'Trata de hablar más alto y claro.', 'Versuche, deutlicher und lauter zu sprechen.', 'Essayez de parler plus clairement et plus fort.'];
+t_recognize_error_second_text = ["Please try to say the phrase in exactly the same way as you heard it. This applies to vocabulary and contractions.", 'Postaraj się wymówić wyrażenie w taki sam sposób w jaki je usłyszałeś (dotyczy to zarówno słownictwa jak i skrótów).', 'Trata de decir la frase exactamente de la misma forma que la has oído (haz lo mismo tanto con el vocabulario como con las contracciones).', 'Bitte wiederhole den Satz genauso, wie du ihn gehört hast. Das gilt auch für Vokabeln und Kontraktionen.', "Essayez de dire la phrase dans le même ordre que vous l'avez entendu (cela s'applique au vocabulaire et aux contractions)."];
+t_recognize_error_third_text = ["Remember sometimes speech recognition is not accurate.", 'Pamiętaj że czasami rozpoznawanie mowy jest niedokładne.', 'Recuerda que a veces el reconocimiento de voz no es preciso.', 'Denke daran, das die Spracherkennung manchmal nicht exakt ist.', "Souvenez- vous que parfois, le système de reconnaissance vocal n'est pas précis. Essayez à nouveau ou passez la tâche."];
+t_end_new_materials_title = ["Congratulations!", "Gratulacje!", "¡Enhorabuena!", "Herzlichen Glückwunsch!", "Félicitations!"];
+t_end_new_materials_text = ["You went through all the content. Now repeat the lessons to become a Pro.", "BRAK TŁUMACZENIA", "Ya has completado todo el contenido. Ahora, repite las lecciones para convertirte en un profesional.", "Du hast den gesamten Inhalt erarbeitet. Wiederhole jetzt die Lektionen, um ein Profi zu werden.", "Vous avez parcouru tout le contenu. Maintenant, répétez les leçons pour devenir un pro."];
+t_end_new_materials_ok = ["OK", "OK", "OK", "OK", "OK"];
+t_liked_words_added = ["Phrase has been added to Favourites.", "Fraza została dodana do ulubionych.", "Frase añadida a Favoritos.", "Der Satz wurde zu deinen Favoriten hinzugefügt.", "La phrase a été ajoutée à vos favoris."];
+t_liked_words_remove = ["BRAK TŁUMACZENIA", "BRAK TŁUMACZENIA", "BRAK TŁUMACZENIA", "BRAK TŁUMACZENIA", "BRAK TŁUMACZENIA"];
+t_no_audio_lesson_title = ["Audio not available.", "Nagrania audio niedostępne.", "Audio no disponible.", "Audio nicht verfügbar.", "L'audio n'est pas disponible."];
+t_no_audio_lesson_text = ["This free version comes with 90 audio lessons. If you would like to get audio to all 180 lessons, upgrade to xxx Pro.", "Darmowa wersja aplikacji zawiera 90 lekcji audio. Jeśli chciałbyś otrzymać nagrania do wszystkich 180 lekcji kup xxx Pro.", "La versión gratuita de la aplicación contiene 90 lecciones de audio. Si deseas recibir las grabaciones de todas las 180 lecciones, compra xxx Pro.", "Diese kostenlose Version umfasst 90 Audiolektionen. Wenn du die Audios für alle 180 Lektionen haben möchtest, kauf dir xxx Pro.", "Cette version gratuite contient 90 leçons audios. Si vous voulez recevoir les audios pour l'ensemble des 180 leçons, achetez xxx Pro."];
+t_no_audio_lesson_learn_anyway = ["Learn anyway", "Ucz się bez nagrań", "Aprender sin grabaciones", "Trotzdem lernen", "Apprenez sans audio"];
+t_no_audio_lesson_upgrade = ["Upgrade", "Kup xxx Pro", "Compra xxx Pro", "xxx Pro kaufen", "Achetez xxx Pro"];
+t_no_audio_lesson_skip = ["Skip lesson", "Pomiń lekcję", "Omitir lección", "Lektion überspringen", "Passez la leçon"];
+t_confirm_back_in_learn_title = ["Are you sure?", "Czy jesteś pewien?", "¿Estás seguro?", "Bist du sicher?", "Etes-vous sûr?"];
+t_confirm_back_in_learn_text = ["Your progress won't be saved.", "Twój postęp nie zostanie zapisany.", "Tu progreso no será guardado.", "Deine Fortschritte werden nicht gespeichert.", "Votre progrès ne sera pas sauvegardé."];
+t_confirm_back_in_learn_back = ["Go back", "Wróć", "Volver", "Zurück", "Revenir en arrière"];
+t_confirm_back_in_learn_cancel = ["Cancel", "Anuluj", "Cancelar", "Abbrechen", "Annulez"];
+t_confirm_learn_missing_cat_title = ["Are you sure?", "Czy jesteś pewien?", "BRAK TŁUMACZENIA", "BRAK TŁUMACZENIA", "BRAK TŁUMACZENIA"];
+t_confirm_learn_missing_cat_text = ["Do you want to learn previously skipped lesson?", "Lekcja została ostatnio pominięta.", "BRAK TŁUMACZENIA", "BRAK TŁUMACZENIA", "BRAK TŁUMACZENIA"];
+t_confirm_learn_missing_cat_yes = ["Learn anyway", "Ucz się", "BRAK TŁUMACZENIA", "BRAK TŁUMACZENIA", "BRAK TŁUMACZENIA"];
+t_confirm_learn_missing_cat_cancel = ["Cancel", "Anuluj", "BRAK TŁUMACZENIA", "BRAK TŁUMACZENIA", "BRAK TŁUMACZENIA"];
+t_confirm_learn_inprogress_cat_title = ["Are you sure?", "Czy jesteś pewien?", "BRAK TŁUMACZENIA", "BRAK TŁUMACZENIA", "BRAK TŁUMACZENIA"];
+t_confirm_learn_inprogress_cat_text = ["You are learning this lesson already.", "Uczysz się już tej lekcji.", "OK", "OK", "OK"];
+t_confirm_learn_inprogress_cat_yes = ["Learn anyway", "Ucz się", "BRAK TŁUMACZENIA", "BRAK TŁUMACZENIA", "BRAK TŁUMACZENIA"];
+t_confirm_learn_inprogress_cat_cancel = ["Cancel", "Anuluj", "BRAK TŁUMACZENIA", "BRAK TŁUMACZENIA", "BRAK TŁUMACZENIA"];
+t_confirm_skip_lesson_title = ["Are you sure?", "Czy jesteś pewien?", "¿Estás seguro?", "Bist du sicher?", "Etes-vous sûr?"];
+t_confirm_skip_lesson_text = ["Repetition is crucial for your long-term memory.", "Powtarzanie jest kluczowe w procesie zapamiętywania.", "La repetición tiene una importancia fundamental para la memorización a largo plazo.", "Wiederholungen sind für das Langzeitgedächtnis sehr wichtig.", "Répéter est essentiel pour la mémoire à long terme."];
+t_confirm_skip_lesson_skip = ["Skip", "Pomiń", "Omitir", "Überspringen", "Passez"];
+t_confirm_skip_lesson_cancel = ["Cancel", "Anuluj", "Cancelar", "Abbrechen", "Annulez"];
+t_confirm_exit_title = ["Are you sure?", "Czy jesteś pewien?", "¿Estás seguro?", "Bist du sicher?", "Etes-vous sûr?"];
+t_confirm_exit_text = ["Please complete the lesson before exiting. Otherwise, your progress won't be saved.", "Ukończ lekcję przed zamknięciem aplikacji.W przeciwnym wypadku postęp nie zostanie zapisany.", "Por favor, completa la lección antes de salir. De lo contrario, tu progreso no será guardado.", "Beende die Lektion, bevor du die Anwendung schließt. Sonst werden deine Fortschritte nicht gespeichert.", "Terminez la leçon avant de fermer l'application. Autrement votre progrès ne sera pas sauvegardé."];
+t_confirm_exit_cancel = ["Cancel", "Anuluj", "Cancelar", "Abbrechen", "Annulez"];
+t_confirm_exit_exit = ["Exit", "Wyjście", "Salir", "Beenden", "Quitter"];
+t_placeholder_fuzzy_search = ["Search for lesson…", "Szukaj lekcji…", "Buscar lección….", "Lektion … suchen", "Chercher la leçon..."];
+t_placeholder_hint_text = ["Click here to write your hint", "PL_Click here to write your hint", "ESP_Click here to write your hint", "DE_Click here to write your hint", "FR_Click here to write your hint"];
+t_lang_rank = ["en", "pl", "es", "de", "fr"];
+
+//trans["a"] = ["OK", "OK", "OK", "OK", "OK"];
+
+/*translacje*/
+function getTrans(key){
+	if(langJSON.lang > 0){
+		return key[parseInt(langJSON.lang-1)];
+	}else{
+		return key[0];
+	}
+}
+
+var isPremium = false;
 var app = {
     // Application Constructor
     initialize: function() {
@@ -17,10 +76,11 @@ var app = {
 	
     onDeviceReady: function() {
 		alert("aaa");
-/*
+
 		var src = '/android_asset/www/date/1.m4a';
 		my_media = new Media(src, function () { }, function (err) { console.log("M: " + err.message + " - " + err.code); });
-		
+
+/*		
 		if(!checkConnection()){	
 			navigator.notification.confirm(
 				getTrans(t_not_connected_text),
@@ -35,9 +95,6 @@ var app = {
 		document.addEventListener("resume", hideBars, false);
 */
 
-		/*var autoHideNavigationBar = true;
-		if($( window ).width() < 600) window.navigationbar.setUp(autoHideNavigationBar); */
-		
 /*		
 		if($( window ).width() > 600) {
 			StatusBar.hide();	
