ff59dc5ac8250fb99d9bcf0b18724f8302c68abf
Robert Przybylski <robert.przybylski.95@gmail.com>
2018-04-05 21:47:14 +0200

    podmiana linków

--- www/js/index.js
+++ www/js/index.js
@@ -3421,7 +3421,7 @@ function showRating() {
 	AppRate.preferences = {
 	  displayAppName: 'SpeakLabs',
 	  storeAppURL: {
-		android: 'market://details?id=com.AwesomeIndustries.DriftZone2'
+		android: 'market://details?id=com.speaklabsPro'
 	  },
 	  useLanguage: getTrans(t_lang_rank),
 	  simpleMode: true,
