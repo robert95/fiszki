b5395fda6786a1689b2482c0f819377d7735ad87
Robert Przybylski <robert.przybylski.95@gmail.com>
2018-04-08 21:38:01 +0200

    up version

--- www/js/admob_simple.js
+++ www/js/admob_simple.js
@@ -6,17 +6,17 @@ var isPremium = true;
 if( /(android)/i.test(navigator.userAgent) ) { 
     admobid = { // for Android
         banner: 'ca-app-pub-5149183983154394/3906975069',
-        interstitial: 'ca-app-pub-5149183983154394/6360913863'
+        interstitial: 'ca-app-pub-5149183983154394/7604074072'
     };
 } else if(/(ipod|iphone|ipad)/i.test(navigator.userAgent)) {
     admobid = { // for iOS
         banner: 'ca-app-pub-6869992474017983/4806197152',
-        interstitial: 'ca-app-pub-6869992474017983/7563979554'
+        interstitial: 'ca-app-pub-5149183983154394/7604074072'
     };
 } else {
     admobid = { // for Windows Phone
         banner: 'ca-app-pub-6869992474017983/8878394753',
-        interstitial: 'ca-app-pub-6869992474017983/1355127956'
+        interstitial: 'ca-app-pub-5149183983154394/7604074072'
     };
 }
 
