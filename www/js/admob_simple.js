0f121f673b0a5d8b7c1619342e20f96c3317bc08
Robert Przybylski <robert.przybylski.95@gmail.com>
2018-11-12 17:25:52 +0100

    pro ver 1.0.12

--- www/js/admob_simple.js
+++ www/js/admob_simple.js
@@ -3,7 +3,7 @@ var admobid = {};
 var firstShownAd = true; 
 var countAd = 0;
 	// PREMIUM CHANGE
-var isPremium = false;
+var isPremium = true;
 if( /(android)/i.test(navigator.userAgent) ) { 
     admobid = { // for Android
         banner: 'ca-app-pub-5149183983154394/3906975069',
